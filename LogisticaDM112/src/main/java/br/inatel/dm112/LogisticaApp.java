d77c4dbff369904deb0e9d4233bf870809625b60
Alexander Augusto <alexaasf1010@gmail.com>
2022-04-10 15:08:50 -0300

    Add project structure for spring boot api

---
+++ LogisticaDM112/src/main/java/br/inatel/dm112/LogisticaApp.java
@@ -0,0 +1,13 @@
+package br.inatel.dm112;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticaApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticaApp.class, args);
+	}
+
+}
